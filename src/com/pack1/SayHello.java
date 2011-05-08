711e550c737c56ee625e401700d6496caaea40c4
austindsouza <austinsdsouza@gmail.com>
2011-05-08 17:02:45 +0530

    ssa

---
+++ src/com/pack1/SayHello.java
@@ -0,0 +1,16 @@
+package com.pack1;
+
+public class SayHello {
+	
+	
+	public void method1(String args)
+	{
+		System.out.println("Hello World");
+	}
+	
+	public void method2(String args)
+	{
+		System.out.println("Hello World1");
+	}
+
+}
